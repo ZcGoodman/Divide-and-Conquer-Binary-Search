fe253f38f4c692f692200b97c81e868a6e8634d3
Zachary Goodman <131831782+ZcGoodman@users.noreply.github.com>
2023-10-16 12:29:33 -0400

    Add files via upload

---
+++ a2_q2.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class a2_q2{
+
+    public static boolean hasDuplicate(int[] arr, int low, int high){
+        if (low>=high){
+            return false;
+        }
+
+        int mid = (low+high) / 2;
+
+        if(arr[mid-1]==arr[mid] || arr[mid+1]==arr[mid]){
+            return true;
+        }
+
+        boolean leftResult = hasDuplicate(arr, low, mid - 1);
+        boolean rightResult = hasDuplicate(arr, mid + 1, high);
+
+
+        return leftResult || rightResult;
+    }
+    public static void main(String[] args) 
+    { 
+        int[] myNum= {1,2,3,4,4,5,6,7,8};
+        boolean ans= hasDuplicate(myNum,0,myNum.length-1);
+
+        System.out.println(ans);
+
+
+
+
+ }
+}
+
